7df2bf77a5b7fab78e4ef6d702904445c169fd0a
SugarLord66 <sad1020420@gmail.com>
2021-03-29 22:22:21 +0800

    quiz1.3

--- quiz1.js
+++ quiz1.js
@@ -1,14 +1,20 @@
-function change() { 
-    var header = document.getElementById("hid123"); 
-    header.innerHTML = "CSIE@CGU"; 
-    
-    var para = document.getElementById("pid123"); 
-    para.innerHTML = "怎麼那麼棒！！."; 
-    
-    } 
-    function myFunction() { 
-    var btn = document.createElement("BUTTON"); 
-    btn.innerHTML = "Change this document"; 
-    document.body.appendChild(btn); 
-    btn.addEventListener("click", change); 
-   } 
+
+function change() {
+    $("#hid123").html("CSIE@CGU");
+    $("#pid123").html("怎麼那麼棒")
+    /*var header = document.getElementById("hid123");
+    header.innerHTML = "CSIE@CGU";
+    var para = document.getElementById("pid123");
+    para.innerHTML = "怎麼那麼棒";*/
+}
+
+function new_button() {
+    var btn="<button id='change'>Change this document</button>";
+    $("body").append(btn);
+    $("#change").click(change);
+    /*var btn = document.createElement("BUTTON");
+    btn.innerHTML = "Change this document";
+    document.body.appendChild(btn);
+    btn.addEventListener("click", change);*/
+}
+$("#btn").click(new_button);
